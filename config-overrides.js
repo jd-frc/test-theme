84bb9f909bea75de6bf84c9f150698a3ffa6419e
Jason DeVelvis <jason@familyroomcoffee.com>
2024-07-02 16:38:45 -0400

    Updated for 2024 (SASS, mainly)

--- config-overrides.js
+++ config-overrides.js
@@ -9,7 +9,23 @@ module.exports = function override(config, env) {
         https: require.resolve('https-browserify'),
         buffer: require.resolve('buffer/'),
         stream: require.resolve('stream-browserify'),
+        "vm": require.resolve("vm-browserify"),
+        process: require.resolve('process/browser'),
+        ...config.resolve.fallback,
     };
+
+    config.ignoreWarnings = [
+      function (warning) {
+        return warning.message.includes("Failed to parse source map");
+      },
+    ];
+
+    config.plugins = (config.plugins || []).concat([
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+      }),
+    ]);
     
     return config;
 }
+const webpack = require('webpack');
